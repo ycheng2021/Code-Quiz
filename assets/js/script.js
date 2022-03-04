b4e382b95c68304e48f624c65cc191289a814c46
Yang Cheng <annacheng09@hotmail.com>
2022-03-03 17:13:18 -0800

    display question and answer options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,64 +1,55 @@
-// countdown timer
-let timer = 100;
-// track highscore 
-let highscore = {
-    score: '',
-    initials: '', 
-}
-// be able to move from current question to next
-let questionCounter = 0;
 // be able to click on start button to begin game
 let startButton = $('.start-btn');
 let startTime = $('.time');
 let mainContainer = $('.container');
-let question = $('.question');
+let questionEl = $('.question');
 let answerChoices = $('.multiple-choices');
+let startText = $('.startText');
+let message = $('.message');
+
+// countdown timer
+let timer = 100;
+// track highscore 
+let highscore = {
+    score: 25,
+    initials: "AC", 
+}
+let isCorrect = false;
+let isWrong = false;
+// be able to move from current question to next
+let questionCounter = 0;
 startTime.text(timer);
+
+let isClicked = false;
 
 // store all question, answers, and correct answer here to call on
 const myQuestions = [
     {
         question: "What HTML tag is used to embed an image in a webpage?",
-        answers: ["A: <background-image>", "B: <img>", "C: <h1>", "D: <p>"],
-        correctAnswer: "myQuestions[0].answers[1]"   
+        answers: ["A: background-image", "B: img", "C: h1", "D: p"],
+        correctAnswer: "B: img"
     },
     {
         question: "What CSS property do we use to change the text color?",
-        answers: [ "A: font-color:", "B: style:", "C: hue:", "D: color:"],
-        correctAnswer: "myQuestions[1].answers[3]"
+        answers: [ "A: font-color", "B: style", "C: hue", "D: color"],
+        correctAnswer: "D: color:"
     },
     {
         question: "What tag is used to create a list with numbers?",
-        answers: [ "A: <el>:", "B: <ol>:", "C: <ul>", "D: <il>"],
-        correctAnswer: "myQuestions[2].answers[1]"
+        answers: [ "A: el", "B: ol", "C: ul", "D: il"],
+        correctAnswer: "B: ol"
     },
     {
         question: "Which CSS property allows an element to stay in the same place even if the page is scrolled?",
         answers: ["A: position:fixed","B: position:relative","C: position:absolute","D: position:static"],
-        correctAnswer: "myQuestions[3].answers[2]"
+        correctAnswer: "C: position:absolute"
     },
     {
         question: "In HTML, what is the lowest level of heading?",
         answers: ["A: h1","B: h3","C: h6","D: h9"],
-        correctAnswer: "myQuestions[4].answers[2]"
+        correctAnswer: "C: h6"
     }
 ]
-
-console.log(myQuestions[0].answers[1])
-
-// set all questions into an array
-let availableQuestions = []
-for (let i=0; i<myQuestions.length; i++) {
-    availableQuestions.push(myQuestions[i].question);
-}
-
-let availableAnswers = []
-for (let i=0; i<5; i++) {
-    availableAnswers.push(myQuestions[i].answers);
-}
-
-console.log(availableQuestions)
-console.log(availableAnswers);
 
 // timer function for score, when game ends, timer stops, keep note of score
 function startTimer() {
@@ -68,28 +59,56 @@ function startTimer() {
 
         // if statement
         // when game ends, stop timer from running
-
-        // call askInitial function
+        if (questionCounter > 5) {
+            clearInterval(timeInterval)
+        }
 
     }, 1000);
 }
 
-function startGame() {
 
-    question.text(availableQuestions[questionCounter]);
+function displayQuestion() {
+    for (let i=0; i<4; i++) {
+        questionEl.text(myQuestions[questionCounter].question)
+        answerChoices.append('<li><button>' + myQuestions[questionCounter].answers[i] + '</button></li>').css('text-align', 'left')
+        answerChoices.children().on('click', function(event) {
+            let selectedAnswer = event.target;
+            if(myQuestions[questionCounter].correctAnswer !== selectedAnswer.value) {
+                timer -= 10;
+                questionCounter++;
+                return 
+            } else {
+                questionCounter++;
+            }
+        })
+        // once user chooses an answer, currentQuestion++
+        // retrieve next set of questions, answers, correct answer from myQuestions
+    }
+}
 
-    answerChoices.append('<li>' + availableAnswers[0] + '</li>')
+function checkAnswer() {
     // for loop, when answer is made
 
     // if answer is correct, add text "Correct" and change message back to visible
 
     // if answer is wrong, add text "Wrong" and change message back to visible
 
-    // once user chooses an answer, currentQuestion++
-    // retrieve next set of questions, answers, correct answer from myQuestions
+}
+  
+// highscore becomes leftover timer
+//store highscores into local storage and get them
 
-    // highscore becomes leftover timer
-    //store highscores into local storage and get them
+function startGame() {
+    startText.text("");
+    startTimer();
+    displayQuestion();
 }
 
-startButton.on('click', startGame());
+function winGame() {
+
+}
+
+startButton.on('click', function() {
+    startButton.remove()
+    startGame()
+});
